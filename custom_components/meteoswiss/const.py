c346716764f7e997ae4936c9c417a0c60a4f4f53
MeteoSwiss Integration <>
2026-02-13 08:15:38 +0000

    chore: Bump version to 3.7.0

--- custom_components/meteoswiss/const.py
+++ custom_components/meteoswiss/const.py
@@ -6,7 +6,7 @@ from typing import Final
 
 DOMAIN: Final = "meteoswiss"
 NAME: Final = "MeteoSwiss"
-VERSION: Final = "3.6.3"
+VERSION: Final = "3.7.0"
 
 # API URLs
 API_BASE: Final = "https://data.geo.admin.ch/api/stac/v1"
